19b1f25958c97798c25cbe153729f58a87218b43
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2022-11-28 12:35:43 +0100

    Added React lazy to app.js component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
+import { Suspense, lazy } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
-import Home from './pages/Home';
-import Aboutus from './pages/Aboutus';
-import Products from './pages/Products';
-import ProductDetails from './pages/ProductDetails';
-
 import Layout from './components/UI/layout/Layout';
+import Loadingspinner from './assets/loadingspinner.gif';
+
+const Home = lazy(() => import('./pages/Home'));
+const Aboutus = lazy(() => import('./pages/Aboutus'));
+const Products = lazy(() => import('./pages/Products'));
+const ProductDetails = lazy(() => import('./pages/ProductDetails'));
 
 const App = () => {
 	return (
 		<Layout>
-			<Routes>
-				<Route path='home' element={<Home />} />
-				<Route path='products' element={<Products />} />
-				<Route path='products/:productId' element={<ProductDetails />} />
-				<Route path='aboutus' element={<Aboutus />} />
-				<Route path={'*' || '/'} element={<Navigate to={'home'} replace />} />
-			</Routes>
+			<Suspense
+				fallback={
+					<img
+						src={Loadingspinner}
+						alt='Loading spinner'
+						style={{ display: 'block', margin: '0 auto' }}
+					/>
+				}
+			>
+				<Routes>
+					<Route path='home' element={<Home />} />
+					<Route path='products' element={<Products />} />
+					<Route path='products/:productId' element={<ProductDetails />} />
+					<Route path='aboutus' element={<Aboutus />} />
+					<Route path={'*' || '/'} element={<Navigate to={'home'} replace />} />
+				</Routes>
+			</Suspense>
 		</Layout>
 	);
 };
