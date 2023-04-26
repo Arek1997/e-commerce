41c862daefbb59c97625a82ee32651b26052a565
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-04-26 09:23:38 +0200

    Add typescript

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import { Suspense, lazy } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Layout from './components/UI/layout/Layout';
-import Loading from './components/Loading/Loading';
+import Loading from './components/loading/Loading';
 
-const Home = lazy(() => import('./pages/Home'));
-const Aboutus = lazy(() => import('./pages/Aboutus'));
-const Products = lazy(() => import('./pages/Products'));
-const ProductDetails = lazy(() => import('./pages/ProductDetails'));
+const Home = lazy(() => import('./pages/home'));
+const Aboutus = lazy(() => import('./pages/aboutus'));
+const Products = lazy(() => import('./pages/product'));
+const ProductDetails = lazy(() => import('./pages/product-details'));
 
 const App = () => {
 	return (
