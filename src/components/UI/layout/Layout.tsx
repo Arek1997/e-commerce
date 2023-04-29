1230db23a5a99a89fe7f5944170315ff24ecc36c
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-04-29 19:46:47 +0200

    Refactor

--- src/components/UI/layout/Layout.tsx
+++ src/components/UI/layout/Layout.tsx
@@ -1,33 +1,20 @@
 import { PropsWithChildren } from 'react';
-import { useAppSelector } from '../../../hooks/reduxHooks';
-
 import Navigation from '../navigation/Navigation';
 import Cart from '../cart/Cart';
-import CartOrder from '../cart/Cart-order';
+import CartOrderWrapper from '../cart/Cart-order';
 import Footer from '../footer/Footer';
-import ProfileAuthModal from '../profile/ProfileAuthModal';
-import ProfileDetails from '../profile/ProfileDetails';
-import ProfileFavProducts from '../profile/ProfileFavProducts';
 import Alert from '../alert/Alert';
+import Profile from '../profile/Profile';
 
 const Layout = (props: PropsWithChildren) => {
-	const {
-		isCartOrderShown,
-		isProfileAuthModalShown,
-		isProfileDetailsShown,
-		ifProfileFavProductsShown,
-	} = useAppSelector((state) => state.navigation);
-
 	return (
 		<>
 			<Navigation />
 			<Cart />
 			<main>
 				{props.children}
-				{isProfileAuthModalShown && <ProfileAuthModal />}
-				{isProfileDetailsShown && <ProfileDetails />}
-				{ifProfileFavProductsShown && <ProfileFavProducts />}
-				{isCartOrderShown && <CartOrder />}
+				<Profile />
+				<CartOrderWrapper />
 				<Alert />
 			</main>
 			<Footer />
