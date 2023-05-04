6bf6929da677dd898bae734bd2cf33b6bd3594ed
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-05-04 18:26:23 +0200

    Update about subpage design

--- src/pages/aboutus/style/styled-aboutus.ts
+++ src/pages/aboutus/style/styled-aboutus.ts
@@ -9,14 +9,55 @@ export const SectionHero = styled.section`
 	${Hero}
 `;
 
-export const SectionCenter = styled.section`
-	padding: 2rem 0;
-	text-align: center;
+export const StyledArticle = styled.article`
+	display: flex;
+	flex-direction: column;
+	gap: 5rem;
+	margin: 0 auto;
 
-	article {
-		max-width: 700px;
-		margin: 0 auto;
+	@media (min-width: 768px) {
+		padding-top: 6rem;
+		padding-bottom: 6rem;
 	}
+
+	@media (min-width: 992px) {
+		flex-direction: row;
+		gap: 6rem;
+		padding-top: 9rem;
+		padding-bottom: 9rem;
+	}
+`;
+
+export const StyledImageWrapper = styled.div`
+	position: relative;
+
+	@media (min-width: 992px) {
+		min-width: 50%;
+	}
+
+	&::before {
+		content: '';
+		position: absolute;
+		inset: 0;
+		background-color: hsl(0deg 2.37% 18.68% / 70%);
+	}
+
+	img {
+		width: 100%;
+		border-radius: 6px;
+
+		@media (min-width: 992px) {
+			height: 100%;
+			object-fit: cover;
+		}
+	}
+`;
+
+export const StyledTextWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: flex-start;
 
 	h3 {
 		${Relative}
@@ -39,7 +80,7 @@ export const SectionCenter = styled.section`
 		line-height: 2;
 
 		@media (min-width: 768px) {
-			font-size: 1.8rem;
+			font-size: 1.7rem;
 		}
 	}
 `;
