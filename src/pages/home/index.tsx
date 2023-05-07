12bf5f05194637210d843d4f41abc8e9de974cf8
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-05-07 16:44:02 +0200

    Add new section

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,13 @@
 import Header from '../../components/UI/header/Header';
 import Featured from '../../components/UI/featured/Featured';
+import UniqueClothes from '../../components/UI/uniqueClothes/UniqueClothes';
 
 const Home = () => {
 	return (
 		<>
 			<Header />
 			<Featured />
+			<UniqueClothes />
 		</>
 	);
 };
