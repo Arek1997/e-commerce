2623471d8cb4bc45f57c9faa385e9007b440a9a7
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-05-14 14:00:07 +0200

    Add helper function

--- src/helpers/functions.ts
+++ src/helpers/functions.ts
@@ -10,3 +10,22 @@ export const getAuthUrl = (isLogin: boolean) => {
 				import.meta.env.VITE_FIREBASE_KEY
 		  }`;
 };
+
+export const checkIfEmailAlreadyExists = async (email: string) => {
+	try {
+		const response = await fetch(
+			`https://alledrogo-e-commerce-default-rtdb.europe-west1.firebasedatabase.app/newsletter-list.json?orderBy="email"&equalTo="${email}"`
+		);
+
+		if (!response.ok) {
+			throw new Error('Something went wrong, try later.');
+		}
+
+		const data = Object.values(await response.json());
+		return data.length ? true : false;
+	} catch (err: any) {
+		const errorMessage = err.message;
+		console.log(errorMessage);
+		throw new Error(errorMessage);
+	}
+};
