ed00b6842d571479e8f285d76ba30f107491ad9c
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-05-14 14:02:11 +0200

    Add email validation before sign up to newsletter

--- src/components/UI/newsLetter/form/Form.tsx
+++ src/components/UI/newsLetter/form/Form.tsx
@@ -6,7 +6,7 @@ import ResponseMessage from '../../../responseMessage/ResponseMessage';
 import { StyledFormWrapper } from './style/styled-form';
 import { EMAIL_REGEXP } from '../../../../helpers/values';
 import Loading from '../../../loading/Loading';
-import { wait } from '../../../../helpers/functions';
+import { wait, checkIfEmailAlreadyExists } from '../../../../helpers/functions';
 
 interface Input {
 	email: string;
@@ -30,6 +30,15 @@ const Form = () => {
 	const newsLetterSubmitHandler: SubmitHandler<Input> = async ({ email }) => {
 		setIsLoading(true);
 		try {
+			const emailExist = await checkIfEmailAlreadyExists(email);
+
+			if (emailExist) {
+				return setResponseMessage({
+					status: 'fail',
+					message: 'This email is already in use.',
+				});
+			}
+
 			const response = await fetch(
 				`https://alledrogo-e-commerce-default-rtdb.europe-west1.firebasedatabase.app/newsletter-list.json`,
 				{
@@ -46,6 +55,7 @@ const Form = () => {
 			}
 
 			await wait(1);
+
 			setResponseMessage({
 				status: 'success',
 				message: 'Thank you for sign up to newsletter.',
@@ -55,9 +65,9 @@ const Form = () => {
 				status: 'fail',
 				message: err.message,
 			});
+		} finally {
+			setIsLoading(false);
 		}
-
-		setIsLoading(false);
 	};
 
 	useEffect(() => {
