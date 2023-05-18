4bfe6c0e2fd6b276e6343be58ce152998659b80e
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-05-18 19:56:51 +0200

    Add inputValidation object

--- src/helpers/values.ts
+++ src/helpers/values.ts
@@ -5,3 +5,22 @@ export const ITEMS_PER_PAGE = 8;
 export const EMAIL_REGEXP = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 export const PASSWORD_REGEXP =
 	/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/gm;
+
+export const inputValidation = {
+	email: {
+		required: 'Email is required',
+		pattern: {
+			message: 'Email is not correct',
+			value: EMAIL_REGEXP,
+		},
+	},
+
+	password: {
+		required: 'Password is required',
+		pattern: {
+			message:
+				'Password have to contains at least 8 sign, at least one uppercase letter, at least one downcase letter, at least one number and at least one special sign.',
+			value: PASSWORD_REGEXP,
+		},
+	},
+};
